e51e8d3be28facebc6e7b798ea5b3c1429773c60
Gabriel Motta <devgmleite@gmail.com>
2025-03-24 20:53:58 -0300

    Started coding JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
 import styles from "./app.module.css"
+import { useEffect, useState } from "react"
+import { WORDS, Challenge } from "./utils/words"
 
-import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
-import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
+import { Letter } from "./components/Letter"
+import { Header } from "./components/Header"
 import { LettersUsed } from "./components/LettersUsed"
 
 export function App() {
-  
+  const [ attempts, setAttempts ] = useState(0)
+  const [ letter, setLetter ] = useState("")
+  const [ challenge, setChallenge ] = useState<Challenge | null>(null)
+
   function handleRestartGame(){
     alert("Reiniciar o Jogo!")
+  }
+
+  function startGame(){
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  if(!challenge){
+    return
   }
   
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame}/>
+        <Header current={attempts} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Dynamic Programmer Language"/>
-        <div className={styles.word}>        
-        <Letter value="R"/>
-        <Letter value="E"/>
-        <Letter value="A"/>
-        <Letter value="C"/>
-        <Letter value="T"/>
+        <div className={styles.word}>
+          {
+            challenge.word.split("").map(() => (
+              <Letter value=""/>
+            ))
+          }        
+        
         </div>
 
         <h4>Palpite</h4>
--- src/utils/words.ts
+++ src/utils/words.ts
@@ -5,13 +5,12 @@ export type Challenge = {
 }
 
 export const WORDS: Challenge[] = [
-  { id: 1, word: "CSS", tip: "Linguagem de estilos" },
-  { id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web" },
-  { id: 3, word: "HTML", tip: "Linguagem de marcação" },
-  {
-    id: 4,
-    word: "Javascript",
-    tip: "Uma das linguagens de programação mais utilizadas no mundo",
-  },
-  { id: 5, word: "Typescript", tip: "Para adicionar tipagem no Javascript" },
+  { id: 1, word: "NARUTO", tip: "O ninja que carrega a raposa de nove caudas" },
+  { id: 2, word: "SASUKE", tip: "Membro do clã Uchiha em busca de vingança" },
+  { id: 3, word: "KAKASHI", tip: "Ex-ANBU e mestre do Time 7" },
+  { id: 4, word: "JIRAIYA", tip: "Escritor e um dos três Sannins lendários" },
+  { id: 5, word: "AKATSUKI", tip: "Grupo de ninjas renegados que buscam capturar as Bijuus" },
+  { id: 6, word: "GAARA", tip: "Jinchuuriki da Areia e futuro Kazekage" },
+  { id: 7, word: "ITACHI", tip: "Uchiha que sacrificou seu clã para proteger Konoha" },
+  { id: 8, word: "MADARA", tip: "Fundador do clã Uchiha e um dos ninjas mais poderosos" },
 ]
